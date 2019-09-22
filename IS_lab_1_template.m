8eed8cd48220de72161d470b9fb7ae8bcc9c5e5c
Darius126 <36037196+Darius126@users.noreply.github.com>
2019-09-22 14:41:35 +0300

    Veikiantis variantas
    Anksčiau buvau nepadaręs fork, dėl to negalėjau publishint senų commit'u. Čia įkeliu jau veikiantį commit.

--- IS_lab_1_template.m
+++ IS_lab_1_template.m
@@ -70,7 +70,7 @@ x2=[metric_A1 metric_A2 metric_A3 metric_P1 metric_P2];
 P=[x1;x2];
 
 %Desired output vector
-T=[1;-1;-1;1;-1];
+T=[1;1;1;-1;-1];
 
 %% train single perceptron with two inputs and one output
 
@@ -80,9 +80,9 @@ w2 = randn(1);
 b = randn(1);
 
 % calculate wieghted sum with randomly generated parameters
-%v1 = <...>; % write your code here
+v1 = x1(1)*w1+x2(1)*w2+b; % write your code here
 % calculate current output of the perceptron 
-if v > 0
+if v1 > 0
 	y = 1;
 else
 	y = -1;
@@ -90,11 +90,12 @@ end
 % calculate the error
 e1 = T(1) - y;
 
-% repeat the same for the rest 4 inputs x1 and x2
+
+
 % calculate wieghted sum with randomly generated parameters
-% v2 = <...> ; % write your code here
+v2 = x1(2)*w1+x2(2)*w2+b; % write your code here
 % calculate current output of the perceptron 
-if v > 0
+if v2 > 0
 	y = 1;
 else
 	y = -1;
@@ -102,31 +103,128 @@ end
 % calculate the error
 e2 = T(2) - y;
 
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v3 = x1(3)*w1+x2(3)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v3 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e3 = T(3) - y;
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v4 = x1(4)*w1+x2(4)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v4 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e4 = T(4) - y;
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v5 = x1(5)*w1+x2(5)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v5 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e5 = T(5) - y;
+
+
+
 % <...> write the code for another 3 inputs
 
 % calculate the total error for these 5 inputs 
 e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
+E = e1 + e2 + e3 + e4 + e5;
 
-% write training algorithm
+n=0.005;
 while e ~= 0 % executes while the total error is not 0
-	% here should be your code of parameter update
-%   calculate output for current example
-% 
-%   calculate error for current example
-% 
-%   update parameters using current inputs ant current error
-% 	w1 = 
-%   w2 = 
-%   b = 
-% 
-%   Test how good are updated parameters (weights) on all examples used for training
-%   calculate outputs and errors for all 5 examples using current values of the parameter set {w1, w2, b}
-%   calculate 'v1', 'v2', 'v3',... 'v5'
-% 
-%   calculate 'y1', ..., 'y5'
-%     
-%   calculate 'e1', ... 'e5'
-    
-	% calculate the total error for these 5 inputs 
-	e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5);
+	
+w1 = w1 + n*abs(e1)*x1(1)+ n*abs(e2)*x1(2)+ n*abs(e3)*x1(3)+n*abs(e4)*x1(4)+n*abs(e5)*x1(5)
+w2 = w1 + n*abs(e1)*x2(1)+ n*abs(e2)*x2(2)+ n*abs(e3)*x2(3)+n*abs(e4)*x2(4)+n*abs(e5)*x2(5)
+b = b + n*E;
+%% 
+
+v1 = x1(1)*w1+x2(1)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v1 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e1 = T(1) - y;
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v2 = x1(2)*w1+x2(2)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v2 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e2 = T(2) - y;
+
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v3 = x1(3)*w1+x2(3)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v3 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e3 = T(3) - y;
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v4 = x1(4)*w1+x2(4)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v4 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e4 = T(4) - y;
+
+
+
+% calculate wieghted sum with randomly generated parameters
+v5 = x1(5)*w1+x2(5)*w2+b; % write your code here
+% calculate current output of the perceptron 
+if v5 > 0
+	y = 1;
+else
+	y = -1;
+end
+% calculate the error
+e5 = T(5) - y;
+
+%% 
+
+
+e = abs(e1) + abs(e2) + abs(e3) + abs(e4) + abs(e5)
+E = e1 + e2 + e3 + e4 + e5;
 end
